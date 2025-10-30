0f8416418ba8a5dee797e042c134321e769b417f
Austin Hogan <austinhogandev@gmail.com>
2025-10-30 09:23:53 -0400

    Initial Commit

---
+++ AuraFlow/AuraFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  AuraFlowApp.swift
+//  AuraFlow
+//
+//  Created by Austin on 10/30/25.
+//
+
+import SwiftUI
+
+@main
+struct AuraFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
